320c3f9f7409767a47372a5afcc81c63d10e8598
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-09-25 13:36:57 -0700

    complete the html for all six quiz questions.

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,18 @@ $(document).ready(function(){
 $("form").submit(function(event){
   name = $("input:radio[name=name]:checked").val();
   bear = $("input:radio[name=bear]:checked").val();
+  music = $("input:radio[name=music]:checked").val();
+  kryptonite = $("input:radio[name=kryptonite]:checked").val();
 
-if (bear === "funshine") {
-  $("#vacationType").append("Hawaii!");
-  $("#result").show();
+
+if (name === "rainbow" && bear === "funshine") {
+  $("#nameOutput").append("Rainbow Dash");
+  $("#result1").show();
 }
+
+
+
+
 event.preventDefault();
   });
 });
