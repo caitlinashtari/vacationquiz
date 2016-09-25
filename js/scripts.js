1b7ff3d6f3661596cf8db0e7198c806028bd5ab5
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-09-24 21:27:17 -0700

    be my first attempt at js and test the functionality of entering a value for care bear and showing hidden result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
+/* Back End */
+var name;
+var bear;
+
+
+/* Front End */
 $(document).ready(function(){
 
+$("form").submit(function(event){
+  name = $("input:radio[name=name]:checked").val();
+  bear = $("input:radio[name=bear]:checked").val();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+if (bear === "funshine") {
+  $("#vacationType").append("Hawaii!");
+  $("#result").show();
+}
+event.preventDefault();
+  });
 });
