ecb58cc64d783f09d970bbe261d68754a98e9635
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-09-25 17:10:10 -0700

    add more branching, add hidden paragraphs for each result.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 var name;
 var bear;
 
-
 /* Front End */
 $(document).ready(function(){
 
@@ -11,16 +10,36 @@ $("form").submit(function(event){
   bear = $("input:radio[name=bear]:checked").val();
   music = $("input:radio[name=music]:checked").val();
   kryptonite = $("input:radio[name=kryptonite]:checked").val();
+  friday = $("input:radio[name=friday]:checked").val();
 
-
-if (name === "rainbow" && bear === "funshine") {
-  $("#nameOutput").append("Rainbow Dash");
-  $("#result1").show();
+if (name){
+  if (name === "Tara Dactyl" || bear === "funshine") {
+      $("span#nameOutput1").append(name);
+      $("#vacationType").append("New Zealand!")
+      $("#result, #newZealandP").show();
+    } else if (name === "Rainbow Dash") {
+        if(bear === "loud"){
+          $("span#nameOutput2").append(name);
+          $("#vacationType").append("Hawaii!")
+          $("#result, #hawaiiP").show();
+    } else {
+      $("span#nameOutput2").append(name);
+      $("#vacationType").append("Hawaii!")
+      $("#result, #hawaiiP").show();
+    }
+  } else if (name === "Michaelangelo") {
+      $("span#nameOutput3").append(name);
+      $("#vacationType").append("Thailand!")
+      $("#result, #thailandP").show();
+  } else if (name === "Steve") {
+      $("span#nameOutput4").append(name);
+      $("#vacationType").append("No vacation for you!")
+      $("#result, #workP").show();
+  }
+  else {
+    alert("Ahem. A name. Please.")
+  }
 }
-
-
-
-
 event.preventDefault();
   });
 });
